39b6ba8bd2ad5dd44c4202506c170dbe5433ada7
Ramon Pintado <ramonpintado91@gmail.com>
2020-05-31 21:14:07 +0200

    fix some bugs and create results folder

--- scrapping.py
+++ scrapping.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 def get_page_results(page: int):
 
-    PARAM_URL = f'http://www.educa.jccm.es/educacion/cm/educa_jccm/BBDD_ACCESS.1.1.tkContent.27265/tkListResults?formName=SQLQueriesSearcher&nshow.sqlResults={page}&position.sqlResults=0&idQuery=961'
+    PARAM_URL = f'http://www.educa.jccm.es/educacion/cm/educa_jccm/BBDD_ACCESS.1.1.tkContent.27265/tkListResults?formName=SQLQueriesSearcher&nshow.sqlResults=3&position.sqlResults={page}&idQuery=961'
     page = requests.get(PARAM_URL)
 
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -29,7 +29,11 @@ def get_page_results(page: int):
         names.append(name.text.strip())
         school_ids.append(school_id.text.strip())
         school_types.append(school_type.text.strip())
-        addresses.append(address.text.strip())
+        if address:
+            addresses.append(address.text.strip())
+        else:
+            addresses.append("")
+
         localities.append(locality.text.strip())
         provinces.append(province.text.strip())
         postal_codes.append(postal_code.text.strip())
@@ -105,4 +109,4 @@ df = pd.DataFrame(
         'Web': webs
     }
 )
-df.to_csv('cam_schools.csv', index=False, encoding='utf-8')
+df.to_csv('results/cam_schools.csv', index=False, encoding='utf-8')
